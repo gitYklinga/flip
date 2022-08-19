3eaa3e7f9fdf2d199faec8ed2c33e9542472a6d5
凌以铿 <yklinga@163.com>
2022-08-19 18:25:30 +0800

    feat: 更新翻页工具, 通过js 生成book内容,只需要配置img图片数组

--- js/flip.js
+++ js/flip.js
@@ -1,39 +1,65 @@
+/**
+ * config: {
+ *    type: '',
+ *    imgs: [],
+ * }
+ * type 需要载入的模式
+ * [row-up(竖版上至下) row-down(竖版下至上) col-left(横版左至右) col-right(横版右至左)]
+ * imgs 有序图片数组 根据顺序生成出所需要的组合图片元素
+ * current 当前所在页数的值
+ */
 class Flip {
-  constructor(type, imgArr) {
-    this.type = type; // 需要载入的模式
-    this.imgArr = imgArr; // 有序的图片数组
+  constructor(config) {
+    this.type = config.type;
+    this.imgs = config.imgs; // 有序的图片数组
+    this.bookClassName = config.bookClassName; // book 类名
+    this.flipClassName = config.flipClassName ? config.flipClassName : "flip"; // flip 类名
+    this.flipCurrent = config.current ? config.current : 0;
     this.current = 1; // 当前值
+    this.book = "";
   }
 
-  getImgList() {
-    console.log(this.imgArr, "imgArr");
+  /**
+   * 初始化 book
+   */
+  initBook() {
+    this.createElement();
+    this.initClickEvent();
   }
 
-  flipStyle() {
-    this.current++;
-  }
-
-  flipRow1() {}
-  flipRow2() {}
-  flipCol1() {}
-  flipCol2() {}
-
-  cDiv(options) {
-    let tdiv = document.createElement("div");
-    options.class ? tdiv.setAttribute("class", options.class) : null;
-    return tdiv;
-  }
-  cImg(options) {
-    let tImg = document.createElement("img");
-    options.src
-      ? tImg.setAttribute("src", options.src)
-      : console.log("存在图片获取失败");
-
-    options.class ? tImg.setAttribute("src", options.class) : null;
-    return tImg;
+  /**
+   * 动态设置属性值
+   * @param {*} el 原元素
+   * @param {*} options 属性配置
+   * @returns 带属性 新元素
+   */
+  setAttr(el, options) {
+    Object.keys(options).forEach((key) => {
+      el.setAttribute(key, options[key]);
+    });
+    return el;
   }
   /**
-   *
+   * 自定义生成 div
+   * @param {class} options 属性值 配置 {class: ''}
+   * @returns
+   */
+  cDiv(options) {
+    let tdiv = document.createElement("div");
+    return this.setAttr(tdiv, options);
+  }
+  /**
+   * 自定义生成 img
+   * @param {src, class} options 属性值 配置 {src: ''}
+   * @returns
+   */
+  cImg(options) {
+    let tImg = document.createElement("img");
+    return this.setAttr(tImg, options);
+  }
+
+  /**
+   * 创建 papers 元素
    * @param {*} pages 子页 已加载图片
    * @param {*} current 自定义当前翻页状态，默认0
    * @param {String} flip 自定义 flip 的class类名 默认 'flip'
@@ -45,23 +71,28 @@ class Flip {
       let tpapers = this.cDiv({ class: "paper" });
       if (current === i) {
         tpapers.classList.add("current");
+      }
+      if (current >= i) {
         tpapers.classList.add(flip);
       }
       tpapers.appendChild(pages[i * 2]);
       tpapers.appendChild(pages[i * 2 + 1]);
       papers.push(tpapers);
     }
-    console.log(papers, "papers");
+    // console.log(papers, "papers");
     return papers;
   }
+  /**
+   * 创建 page 元素
+   * @returns
+   */
   createPage() {
-    console.log(this.imgArr);
     let pages = [];
 
-    if (!this.imgArr.length) return console.log("缺少传入图片集"); // 判断是否传入图片
+    if (!this.imgs.length) return console.log("缺少传入图片集"); // 判断是否传入图片
 
-    for (let i = 0; i < this.imgArr.length; i++) {
-      const url = this.imgArr[i];
+    for (let i = 0; i < this.imgs.length; i++) {
+      const url = this.imgs[i];
       const cImg = this.cImg({ src: url });
       if (i % 2 === 0) {
         let front = this.cDiv({ class: "page front" });
@@ -73,14 +104,48 @@ class Flip {
         pages.push(back);
       }
     }
-    console.log(pages, "pages");
+    // console.log(pages, "pages");
     return pages;
   }
+
+  /**
+   * 创建 book 元素
+   */
   createElement() {
-    var book = this.cDiv({ class: "book" });
+    var book = this.cDiv({ class: this.bookClassName });
     let pages = this.createPage();
-    let papers = this.createPapers(pages);
+    let papers = this.createPapers(pages, this.flipCurrent, this.flipClassName);
     papers.forEach((el) => book.appendChild(el));
-    document.body.appendChild(book);
+    document.querySelector(".book-container").appendChild(book);
+    this.book = book;
+  }
+
+  /**
+   * 创建 book click 事件
+   */
+  initClickEvent() {
+    this.book.addEventListener("click", () => {
+      var papers = this.book.querySelectorAll(".paper");
+      var curentPapper = papers[this.current];
+      // console.log(papers, curentPapper, this.current, "my paper");
+      curentPapper.classList.add("current");
+      if (this.type === "col-down") {
+        curentPapper.classList.remove(this.flipClassName);
+      } else {
+        curentPapper.classList.add(this.flipClassName);
+      }
+
+      Array.from(papers).forEach((paper, index) => {
+        if (index !== this.current) {
+          paper.classList.remove("current");
+        }
+      });
+
+      if (this.type === "col-down") {
+        this.current -= 1;
+      } else {
+        this.current += 1;
+      }
+    });
   }
 }
--- js/index.js
+++ js/index.js
@@ -1,23 +1,157 @@
 function flipRow1() {
+  var imgContainer = document.querySelector(".img-container");
+  var imgsChild = document.querySelector(".img-container").children;
+  var rowLen = imgsChild.length ? imgsChild.length : 0;
+  var rowImgs = {};
+
+  for (let i = 0; i < imgsChild.length; i++) {
+    let node = imgsChild[i];
+    rowImgs[`arr${i}`] = [];
+    for (let j = 0; j < node.children.length; j++) {
+      let cnode = node.children[j];
+      rowImgs[`arr${i}`].push(cnode.src);
+    }
+  }
+  console.log(rowImgs, "rowimgs");
+
   var imgarr = [
-    "./img/z1.png",
-    "./img/z1.png",
-    "./img/z2.png",
-    "./img/tt2.png",
-    "./img/tt1.png",
-    "./img/tt1.png",
+    "./imgline/p1-1.png",
+    "./imgline/p1-1.png",
+    "./imgline/p1-2.png",
+    "./imgline/p2-1.png",
+    "./imgline/p2-2.png",
+    "./imgline/p2-2.png",
   ];
-  var row1 = new Flip("row", imgarr);
-  row1.createElement();
-  console.log(row1, "www");
+
+  var imgarr1 = [
+    "./imgline/p2-4.png",
+    "./imgline/p2-4.png",
+    "./imgline/p2-3.png",
+    "./imgline/p3-4.png",
+    "./imgline/p3-3.png",
+    "./imgline/p3-3.png",
+  ];
+
+  var imgarr2 = [
+    "./imgline/p3-2.png",
+    "./imgline/p3-2.png",
+    "./imgline/p3-1.png",
+    "./imgline/p4-2.png",
+    "./imgline/p4-1.png",
+    "./imgline/p4-1.png",
+  ];
+
+  var imgarr3 = [
+    "./imgline/p5-3.png",
+    "./imgline/p5-4.png",
+    "./imgline/p5-3.png",
+    "./imgline/p4-4.png",
+    "./imgline/p4-3.png",
+    "./imgline/p4-3.png",
+  ];
+
+  var bookConf = {
+    type: "row-up",
+    imgs: imgarr,
+    bookClassName: "book",
+  };
+
+  var book1Conf = {
+    type: "col-up",
+    imgs: imgarr1,
+    bookClassName: "book1 book-hide",
+    flipClassName: "flip1",
+  };
+
+  var book2Conf = {
+    type: "row-down",
+    imgs: imgarr2,
+    bookClassName: "book2 book-hide",
+    flipClassName: "flip2",
+  };
+
+  var book3Conf = {
+    type: "col-down",
+    imgs: imgarr3,
+    bookClassName: "book3 book-hide",
+    flipClassName: "flip3",
+    current: 1,
+  };
+
+  var row = new Flip(bookConf);
+  var row1 = new Flip(book1Conf);
+  var row2 = new Flip(book2Conf);
+  var row3 = new Flip(book3Conf);
+
+  row.initBook();
+  row1.initBook();
+  row2.initBook();
+  row3.initBook();
 }
+
 function flipRow2() {
   now += 1;
 }
 function flipCol1() {}
 function flipCol2() {}
 
+function baseClick() {
+  var base = document.querySelector(".base");
+  var base1 = document.querySelector(".base1");
+  var base2 = document.querySelector(".base2");
+  var base3 = document.querySelector(".base3");
+  var base4 = document.querySelector(".base4");
+
+  var book = document.querySelector(".book");
+  var book1 = document.querySelector(".book1");
+  var book2 = document.querySelector(".book2");
+  var book3 = document.querySelector(".book3");
+
+  base.addEventListener("click", function () {
+    var el = this;
+    el.classList.add("base-hide");
+    book.click();
+    setTimeout(() => {
+      book.classList.add("book-hide");
+      base1.classList.remove("base-hide");
+      book1.classList.remove("book-hide");
+    }, 2000);
+  });
+
+  base1.addEventListener("click", function () {
+    var el = this;
+    el.classList.add("base-hide");
+    book1.click();
+    console.log("base1 click");
+    setTimeout(() => {
+      book1.classList.add("book-hide");
+      base2.classList.remove("base-hide");
+      book2.classList.remove("book-hide");
+    }, 2000);
+  });
+
+  base2.addEventListener("click", function () {
+    var el = this;
+    el.classList.add("base-hide");
+    book2.click();
+    setTimeout(() => {
+      book2.classList.add("book-hide");
+      base3.classList.remove("base-hide");
+      book3.classList.remove("book-hide");
+    }, 2000);
+  });
+
+  base3.addEventListener("click", function () {
+    var el = this;
+    el.classList.add("base-hide");
+    book3.click();
+    setTimeout(() => {
+      book3.classList.add("book-hide");
+      base4.classList.remove("base-hide");
+    }, 2000);
+  });
+}
 window.onload = function () {
-  console.log(123);
   flipRow1();
+  baseClick();
 };
